be1bf7adcd01f9694f4da28a3e4adf89a8cdb571
Louis Tur <louis.tur@gmail.com>
2015-05-13 01:42:27 -0400

    Adds solution, plus proposed extra credit and tests for each

--- swift-wwdc-badges/AppDelegate.swift
+++ swift-wwdc-badges/AppDelegate.swift
@@ -8,38 +8,56 @@
 
 import UIKit
 
+
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+public class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    public var window: UIWindow?
+    public let conferenceSpeakers: [String] = ["Edsger", "Ada", "Charles", "Grace", "Linus", "Wozniak", "Alan"]
+    
+    public func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-
-
+        
+        let badges = printBadges(forGuests: conferenceSpeakers)
+        
+        for badge in badges{
+            println(badge)
+        }
+        
         return true
     }
+    
+    public func printBadges(forGuests guests: [String], withMessage message: String = "Hello, my name is") -> [String] {
+        
+        var guestBadges: [String] = []
+        
+        for guest in guests {
+            guestBadges.append("\(message) \(guest)")
+        }
+        
+        return guestBadges
+    }
 
-    func applicationWillResignActive(application: UIApplication) {
+    public func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    public func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    public func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    public func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    public func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
--- swift-wwdc-badgesTests/swift_wwdc_badgesTests.swift
+++ swift-wwdc-badgesTests/swift_wwdc_badgesTests.swift
@@ -8,29 +8,74 @@
 
 import UIKit
 import XCTest
+import swift_wwdc_badges
 
 class swift_wwdc_badgesTests: XCTestCase {
     
+    let badgeString: String = "Hello, my name is"
+    
+    let expectedSpeakers: [String] = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Wozniak"]
+    let unexpectedSpeakers: [String] = ["Bill Clinton", "John Forstall", "Wayne Brady"]
+    
+    var testAppDelegate: AppDelegate?
+    var testingSpeakers: [String]?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testAppDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        testingSpeakers = testAppDelegate!.conferenceSpeakers
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testThatAppDelegateExists() {
+        XCTAssertNotNil(testAppDelegate, "AppDelegate should exist")
+        XCTAssertNotNil(testingSpeakers, "AppDelegate should have speakers")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testArrayMembership() {
+        
+        let invitedSpeakers: NSArray = expectedSpeakers.sorted { $0 > $1 }
+        let uninvitedSpeakers: NSArray = unexpectedSpeakers.sorted { $0 > $1 }
+        
+        let actualSpeakers: NSArray = testingSpeakers!.sorted { $0 > $1 }
+
+        XCTAssertTrue(invitedSpeakers.isEqualToArray(actualSpeakers as! [String]), "It should have the correct speakers")
+        XCTAssertFalse(uninvitedSpeakers.isEqualToArray(actualSpeakers as! [String]), "It should not have uninvited speakers")
+    
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func testForTotalAttendants() {
+        
+        let correctCount = expectedSpeakers.count
+        let actualCount = testingSpeakers!.count
+        
+        XCTAssertEqual(correctCount, actualCount, "It should return the correct number of speakers")
+        
+    }
+    
+    func testDefaultStringExtraCredit() {
+        
+        var expectedDefaultBadgesMade: [String] = []
+        for guest in expectedSpeakers {
+            expectedDefaultBadgesMade.append("\(badgeString) \(guest)")
         }
+        
+        let actualDefaultBadgesMade = testAppDelegate!.printBadges(forGuests: testAppDelegate!.conferenceSpeakers).sorted{ $0 > $1 }
+        let expectedAndSortedBadges = expectedDefaultBadgesMade.sorted{ $0 > $1 }
+        
+        XCTAssertTrue(actualDefaultBadgesMade == expectedAndSortedBadges, "It should generate the correct badges for a defaulted string")
+    }
+    
+    func testExplicitStringExtraCredit() {
+        
+        var expectedDefaultBadgesMade: [String] = []
+        for guest in expectedSpeakers {
+            expectedDefaultBadgesMade.append("\(badgeString) \(guest)")
+        }
+        
+        let actualDefaultBadgesMade = testAppDelegate!.printBadges(forGuests: testAppDelegate!.conferenceSpeakers, withMessage: badgeString).sorted{ $0 > $1 }
+        let expectedAndSortedBadges = expectedDefaultBadgesMade.sorted{ $0 > $1 }
+
+        XCTAssertTrue(actualDefaultBadgesMade == expectedAndSortedBadges, "It should generate the correct badges when explicitly passing a message")
     }
     
 }
